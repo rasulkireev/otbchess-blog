c1b1c87477a76e0c9fd5cf682a392d7a8c597207
Rasul Kireev <rasul.kireev@guycarp.com>
2021-06-11 23:18:21 -0400

    styled the blog post component

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -16,17 +16,21 @@ export default function Template({ data }: Props) {
       <Seo title={`${post.frontmatter.title}`} />
       <Helmet title={`${post.frontmatter.title}`} />
 
-        <div className="relative px-4 sm:px-6 lg:px-8">
-          <div className="mx-auto text-lg max-w-prose">
-            <h1 className="block mt-2 text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900 sm:text-4xl">
-              {post.frontmatter.title}
-            </h1>
-          </div>
-          <div
-            className="mx-auto mt-6 prose prose-lg text-gray-500 prose-indigo"
-            dangerouslySetInnerHTML={{ __html: post.html }}
-          />
-        </div>
+
+      <div className="pt-16 pb-8 mx-auto mt-1 text-gray-900 sm:px-4">
+        <p className="mb-10 text-blue-700">← <Link to="/">back to posts</Link></p>
+        <h1 className="mb-4 text-4xl font-extrabold sm:text-5xl sm:tracking-tight">
+          {post.frontmatter.title}
+        </h1>
+        <p className="text-gray-500">
+          {post.frontmatter.author} | {post.frontmatter.date} | {post.timeToRead} min
+        </p>
+      </div>
+
+      <div
+        className="mx-auto mt-6 prose prose-lg text-gray-800 prose-indigo"
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
     </Layout>
   )
 }
@@ -35,10 +39,12 @@ export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
+      timeToRead
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        author
       }
     }
   }
